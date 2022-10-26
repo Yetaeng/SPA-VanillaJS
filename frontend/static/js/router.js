cba3dcf7adf7c280388afb1cdbec21fecb0d42d7
yetaeng <yetaeng20@gmail.com>
2022-10-26 14:18:49 +0900

    remove remark & update router function

--- frontend/static/js/router.js
+++ frontend/static/js/router.js
@@ -3,40 +3,27 @@ import About from '../views/about.js';
 import Dashboard from '../views/dashboard.js';
 import NotFound from '../views/notFound.js';
 
-const router = async () => {
-  const routes = [
-    { path: '/', view: Main},
-    { path: '/about', view: About},
-    { path: '/dashboard', view: Dashboard},
-  ];
-  
-  // routes 배열을 돌며 현재 브라우저 url의 /뒤와 일치하는 route를 찾아 새로운 배열 potentialMatches을 반환함
-  const potentialMatches = routes.map(route => {
-    return {  
-      route,
-      isMathch: route.path === location.pathname
-    };
-  });
-  
-  let match = potentialMatches.find(match => match.isMathch);
-  if (!match) {
-    const view = new NotFound();
-    view.render(document.querySelector('body'));
-  } else {
-    const view = new match.route.view();
-    view.render(document.querySelector('#app'));
-  };
+const routes = [
+  { path: '/', view: Main},
+  { path: '/about', view: About},
+  { path: '/dashboard', view: Dashboard},
+];
+
+const router = () => {
+
+  const foundRoute = routes.find(route => route.path === location.pathname)?.view || NotFound;
+
+  let currentView = new foundRoute();
+  currentView.render(document.querySelector('#app'));
 };
 
-// 페이지 이동
 const navigator = (url) => {
-  window.history.pushState(null, null, url); // url 주소 바꿔줌. 렌더링은 x
+  window.history.pushState(null, null, url);
 
   router();
 }
 
-// 뒤로가기, 앞으로가기 버튼 동작시키기
-window.addEventListener("popstate", router);
+window.addEventListener("popstate", router());
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('#navigation').addEventListener("click", (e) => {
